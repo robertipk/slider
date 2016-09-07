f5b18d534db5e5a566c595fb706829e0f6a6fe6d
Robert Ip <robertip@alumni.rice.edu>
2016-09-06 22:57:45 -0400

    minor changes

--- solver.rb
+++ solver.rb
@@ -16,6 +16,7 @@ def astar_search(new_game)
     if board.isSolved
       puts "Number of nodes expanded: " << num_Nodes_Expanded.to_s
       puts "Time taken: " << (Time.now-start).to_s
+      board.display_history
       break
     end
     x_coord = board.index_zero.first
@@ -68,78 +69,80 @@ def dfs(new_game)
   puts "-----------------Executing depth first search--------------------"
   start = Time.now
   num_Nodes_Expanded = 0
-   visited_states = Hash.new
-   stack = Array.new
-   stack << new_game
-   while !stack.empty?
-     bo = stack.pop
-     if bo.isSolved
-       puts "Number of nodes expanded: " << num_Nodes_Expanded.to_s
-       puts "Time taken: " << (Time.now-start).to_s
-       break
-     end
-     # mark this permutation of the board as already visited
-     visited_states[bo.get_board] = true
+  visited_states = Hash.new
+  stack = Array.new
+  stack << new_game
+  while !stack.empty?
+    bo = stack.pop
+    num_Nodes_Expanded += 1
+    if bo.isSolved
+     puts "Number of nodes expanded: " << num_Nodes_Expanded.to_s
+     puts "Time taken: " << (Time.now-start).to_s
+     bo.display_history
+     break
+    end
 
-     x_coord = bo.index_zero.first
-     y_coord = bo.index_zero[1]
 
-     # add adjacent nodes to the stack IFF these nodes have not yet been visited
-     # each time a node is added to the stack, increment visited_nodes
+    # and push its adjacent nodes onto the stack
+    if !visited_states.has_key?(bo.get_board)
+      visited_states[bo.get_board] = true
 
-     # move left if possible and resulting state has not been visited yet
-     if y_coord != 0 && bo.get_history.last != "right"
-       copyb = copy_board(bo.get_board)
-       copyh = copy_history(bo.get_history)
-       state_copy = State.new(copyb, copyh)
-       state_copy.moveLeft(x_coord,y_coord)
-       key = state_copy.get_board
-       if !visited_states.has_key?(key)
+      x_coord = bo.index_zero.first
+      y_coord = bo.index_zero[1]
+
+      # expand the node and add the new nodes to the queue
+      # as long as they're not in the frontier and haven't yet been visited
+
+      # move left if possible and resulting state has not been visited yet
+      if y_coord != 0 && bo.get_history.last != "right"
+        copyb = copy_board(bo.get_board)
+        copyh = copy_history(bo.get_history)
+        state_copy = State.new(copyb, copyh)
+        state_copy.moveLeft(x_coord,y_coord)
+        key = state_copy.get_board
+        if !visited_states.has_key?(key)
          visited_states[:key] = true
          stack << state_copy
-         num_Nodes_Expanded += 1
+        end
+      end
+
+      # move right if possible and resulting state has not been visited yet
+       if y_coord != 2 && bo.get_history.last != "left"
+         copyb = copy_board(bo.get_board)
+         copyh = copy_history(bo.get_history)
+         state_copy = State.new(copyb, copyh)
+         state_copy.moveRight(x_coord,y_coord)
+         key = state_copy.get_board
+         if !visited_states.has_key?(key)
+           visited_states[:key] = true
+           stack << state_copy
+         end
        end
-     end
 
-    # move right if possible and resulting state has not been visited yet
-     if y_coord != 2 && bo.get_history.last != "left"
-       copyb = copy_board(bo.get_board)
-       copyh = copy_history(bo.get_history)
-       state_copy = State.new(copyb, copyh)
-       state_copy.moveRight(x_coord,y_coord)
-       key = state_copy.get_board
-       if !visited_states.has_key?(key)
-         visited_states[:key] = true
-         stack << state_copy
-         num_Nodes_Expanded += 1
+       # move up if possible and resulting state has not been visited yet
+       if x_coord != 0 && bo.get_history.last != "down"
+         copyb = copy_board(bo.get_board)
+         copyh = copy_history(bo.get_history)
+         state_copy = State.new(copyb, copyh)
+         state_copy.moveUp(x_coord,y_coord)
+         key = state_copy.get_board
+         if !visited_states.has_key?(key)
+           visited_states[:key] = true
+           stack << state_copy
+         end
        end
-     end
 
-     # move up if possible and resulting state has not been visited yet
-     if x_coord != 0 && bo.get_history.last != "down"
-       copyb = copy_board(bo.get_board)
-       copyh = copy_history(bo.get_history)
-       state_copy = State.new(copyb, copyh)
-       state_copy.moveUp(x_coord,y_coord)
-       key = state_copy.get_board
-       if !visited_states.has_key?(key)
-         visited_states[:key] = true
-         stack << state_copy
-         num_Nodes_Expanded += 1
-       end
-     end
-
-     # move down if possible and resulting state has not been visited yet
-     if x_coord != 2 && bo.get_history.last != "up"
-       copyb = copy_board(bo.get_board)
-       copyh = copy_history(bo.get_history)
-       state_copy = State.new(copyb, copyh)
-       state_copy.moveDown(x_coord,y_coord)
-       key = state_copy.get_board
-       if !visited_states.has_key?(key)
-         visited_states[:key] = true
-         stack << state_copy
-         num_Nodes_Expanded += 1
+       # move down if possible and resulting state has not been visited yet
+       if x_coord != 2 && bo.get_history.last != "up"
+         copyb = copy_board(bo.get_board)
+         copyh = copy_history(bo.get_history)
+         state_copy = State.new(copyb, copyh)
+         state_copy.moveDown(x_coord,y_coord)
+         key = state_copy.get_board
+         if !visited_states.has_key?(key)
+           visited_states[:key] = true
+           stack << state_copy
+         end
        end
      end
   end
@@ -153,13 +156,17 @@ def bfs(new_game)
   q << new_game
   while (!q.empty?)
     bo = q.pop
+    num_Nodes_Expanded += 1
     if bo.isSolved
       puts "Number of nodes expanded: " << num_Nodes_Expanded.to_s
       puts "Time taken: " << (Time.now-start).to_s
+      bo.display_history
       break
     end
     x_coord = bo.index_zero.first
     y_coord = bo.index_zero[1]
+
+    # expand the node and add the new nodes to the queue
 
     # move left if possible
     if y_coord != 0 && bo.get_history.last != "right"
@@ -168,7 +175,6 @@ def bfs(new_game)
       state_copy = State.new(copyb, copyh)
       state_copy.moveLeft(x_coord,y_coord)
       q.push(state_copy)
-      num_Nodes_Expanded += 1
     end
 
     # move right if possible
@@ -178,7 +184,6 @@ def bfs(new_game)
       state_copy = State.new(copyb, copyh)
       state_copy.moveRight(x_coord,y_coord)
       q.push(state_copy)
-      num_Nodes_Expanded += 1
     end
 
     # move up if possible
@@ -188,7 +193,6 @@ def bfs(new_game)
       state_copy = State.new(copyb, copyh)
       state_copy.moveUp(x_coord,y_coord)
       q.push(state_copy)
-      num_Nodes_Expanded += 1
     end
 
     # move down if possible
@@ -198,12 +202,11 @@ def bfs(new_game)
       state_copy = State.new(copyb, copyh)
       state_copy.moveDown(x_coord,y_coord)
       q.push(state_copy)
-      num_Nodes_Expanded += 1
     end
   end
 end
 
 new_game = State.new([[1,5,8],[4,2,7],[0,3,6]],[])
 astar_search(new_game)
-dfs(new_game)
+# dfs(new_game)
 bfs(new_game)
