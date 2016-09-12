d15f4e47caf3272035097d2a395633bff0b5d774
Robert Ip <robertip@alumni.rice.edu>
2016-09-12 10:48:40 -0400

    'checkin'

--- puzzle.rb
+++ puzzle.rb
@@ -6,7 +6,6 @@ class State
   def read_file(file_name)
     text = File.open(file_name).read
     @board = make_board(text)
-
   end
 
   def make_board(text)
@@ -34,15 +33,24 @@ class State
     return new_board
   end
 
+  def initialize(board,history)
+    if board.length == 3
+      return initialize_3(board,history)
+    elsif board.length == 4
+      return initialize_4(board,history)
+    end
+  end
+
   # initialize a 4 x 4 slider puzzle
   def initialize_4 (board,history)
     # instance variables
     @board = board
     @history = history
     @goal_state = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]]
-    @onex = @twox = @threey = @sixy = 0
-    @oney = @threex = @fourx = @foury = @fivex = @seveny = 1
-    @twoy = @fivey = @sevenx = @eightx = @sixx = @eighty = 2
+    @onex = @twox = @threex = @foury = @eighty = @twelvey = 0
+    @oney = @fourx = @fivex = @fivey = @sixx = @sevenx = @niney = @thirteeny = 1
+    @twoy = @sixy = @eightx = @ninex = @tenx = @teny = @elevenx = @fourteeny = 2
+    @threey = @seveny = @eleveny = @twelvex = @thirteenx = @fourteenx = @fifteenx = @fifteeny = 3
   end
 
   # initialize a 3 x 3 slider puzzle
@@ -97,7 +105,7 @@ class State
 
   ## swaps the blank tile with the tile above it
   def moveDown(x,y)
-   if x != 2
+   if x != @board.length-1
     @history << "down"
     @board[x][y],@board[x+1][y] = @board[x+1][y],@board[x][y]
    end
@@ -121,7 +129,7 @@ class State
 
   ## swaps the blank tile with the tile to its right
   def moveRight(x,y)
-   if y != 2
+   if y != @board.length
      @history << "right"
      @board[x][y],@board[x][y+1] = @board[x][y+1],@board[x][y]
    end
@@ -129,7 +137,7 @@ class State
 
   # finds x and y coordinates of an element in the 2D board
   def find_index(num)
-    for x in 0...3
+    for x in 0...@board.length
       if @board[x].index(num) != nil
         x_coord = x
         y_coord = @board[x].index(num)
@@ -139,6 +147,7 @@ class State
   end
 
   # the Manhattan distance is the sum of the absolute values of the horizontal and the vertical distance
+  # for a 3 x 3 board
   def get_manhattan_distance(num)
     case num
     when 1
@@ -165,7 +174,29 @@ class State
     when 8
       coords = find_index(8)
       return (coords[0]-@eightx).abs + (coords[1]-@eighty).abs
+    when 9
+      coords = find_index(9)
+      return (coords[0]-@ninex).abs + (coords[1]-@niney).abs
+    when 10
+      coords = find_index(10)
+      return (coords[0]-@tenx).abs + (coords[1]-@teny).abs
+    when 11
+      coords = find_index(11)
+      return (coords[0]-@elevenx).abs + (coords[1]-@eleveny).abs
+    when 12
+      coords = find_index(12)
+      return (coords[0]-@twelvex).abs + (coords[1]-@twelvey).abs
+    when 13
+      coords = find_index(13)
+      return (coords[0]-@thirteenx).abs + (coords[1]-@thirteeny).abs
+    when 14
+      coords = find_index(14)
+      return (coords[0]-@fourteenx).abs + (coords[1]-@fourteeny).abs
+    when 15
+      coords = find_index(15)
+      return (coords[0]-@fifteenx).abs + (coords[1]-@fifteeny).abs
     end
+
   end
 
   # returns the g(n) part of the cost function, which is the amount of steps
